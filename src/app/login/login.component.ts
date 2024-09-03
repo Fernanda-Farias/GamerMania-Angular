27b0739bca376d07cd664ea6d019aedb3beb5cfa
Fernanda Farias <contato.ffarias@gmail.com>
2024-09-02 21:14:31 -0300

    Add TS Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuario: string = '';
+  senha: string = '';
+  mensagemErro: string = '';
 
+  login(): void {
+
+    if (!this.usuario || !this.senha) {
+      this.mensagemErro = 'Preencha os campos para efetuar o login';
+      return;
+    }
+
+    if (this.usuario === 'Fernanda' && this.senha === '123') {
+      alert('Login feito com sucesso!');
+      window.location.href = 'index.html';
+    } else {
+      this.mensagemErro = 'Usuário ou senha incorretos.';
+    }
+  }
 }
